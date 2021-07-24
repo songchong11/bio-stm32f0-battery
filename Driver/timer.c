56a0cf0e0077c8e9d9d55dbb10e5b751c250e196
songchong <1972433980@qq.com>
2021-07-25 01:31:07 +0800

    确定定时器的时钟为48MHZ

---
+++ Driver/timer.c
@@ -0,0 +1,63 @@
+#include "timer.h"
+#include "led.h"
+#include "usart.h"
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//ALIENTEK战舰STM32开发板
+//定时器 驱动代码	   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2012/9/3
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 广州市星翼电子科技有限公司 2009-2019
+//All rights reserved									  
+//////////////////////////////////////////////////////////////////////////////////   	 
+
+//通用定时器14中断初始化
+//这里时钟选择为APB1的2倍，而APB1为24M
+//arr：自动重装值。
+//psc：时钟预分频数
+//这里使用的是定时器14!   48MHZ
+void TIM14_Int_Init(u16 arr,u16 psc)
+{
+	TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM14, ENABLE); //时钟使能
+	
+	//定时器TIM3初始化
+	TIM_TimeBaseStructure.TIM_Period = arr; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值	
+	TIM_TimeBaseStructure.TIM_Prescaler =psc; //设置用来作为TIMx时钟频率除数的预分频值
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1; //设置时钟分割:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	TIM_TimeBaseInit(TIM14, &TIM_TimeBaseStructure); //根据指定的参数初始化TIMx的时间基数单位
+ 
+	TIM_ITConfig(TIM14,TIM_IT_Update,ENABLE ); //使能指定的TIM3中断,允许更新中断
+
+	//中断优先级NVIC设置
+	NVIC_InitStructure.NVIC_IRQChannel = TIM14_IRQn;  //TIM3中断
+	NVIC_InitStructure.NVIC_IRQChannelPriority = 0;  //先占优先级0级
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
+	NVIC_Init(&NVIC_InitStructure);  //初始化NVIC寄存器
+
+
+	TIM_Cmd(TIM14, ENABLE);  //使能TIMx					 
+}
+//定时器14中断服务程序
+void TIM14_IRQHandler(void)   //TIM3中断
+{
+	if (TIM_GetITStatus(TIM14, TIM_IT_Update) != RESET)  //检查TIM14更新中断发生与否
+		{
+				TIM_ClearITPendingBit(TIM14, TIM_IT_Update  );  //清除TIMx更新中断标志 
+				LED_G = !LED_G;
+				LED_B = !LED_B;
+				printf("test\r\n");
+		}
+}
+
+
+
+
+
+
