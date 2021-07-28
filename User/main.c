52a8dfb3e613f9267312a29d73bb4845be4063c1
songchong <1972433980@qq.com>
2021-07-29 01:47:33 +0800

    调试采集电阻值

--- User/main.c
+++ User/main.c
@@ -6,6 +6,8 @@
 #include "mb.h"
 #include "timer.h"
 #include "adc.h"
+#include <string.h>
+
 
 uint8_t slave_address = 0x00;
 
@@ -17,8 +19,18 @@ extern uint8_t sample_finish;
 extern uint16_t adc_value[5]; 
 
 extern uint16_t usRegHoldingBuf[REG_HOLDING_NREGS];
+extern uint16_t usRegInputBuf[REG_INPUT_NREGS];
 
 volatile int msg_type;
+
+union volt{
+	unsigned char volt_v[4];
+	float volt_f;
+};
+
+static union volt voltage[3];
+
+static union volt f_test;
 
 //show you a blink
 int main(void)
@@ -41,6 +53,16 @@ int main(void)
 	printf("slave_address is %d\r\n\r\n", slave_address);
 	
 	printf("bio-battery init ok start main...\r\n");
+	
+#if 0
+	//this is ok
+	f_test.volt_f = 3.2999;
+	f_test.volt_v[1] = 0x26;
+	f_test.volt_v[0] = 0x00;
+	f_test.volt_v[3] = 0x40;
+	f_test.volt_v[2] = 0x53;
+#endif
+
 
     while (1)
     {
@@ -56,11 +78,22 @@ int main(void)
         if (sample_finish == 1) {
 			//printf(" %x %x %x %x %x\r\n", adc_value[0], adc_value[1], adc_value[2], adc_value[3], adc_value[4]);
 
-			volt_0 = (float)adc_value[2]*(3.3/4096);
-			volt_1 = (float)adc_value[3]*(3.3/4096);
-			volt_2 = (float)adc_value[4]*(3.3/4096);
+			voltage[0].volt_f = (float)adc_value[2]*(3.3/4096);
+			voltage[1].volt_f = (float)adc_value[3]*(3.3/4096);
+			voltage[2].volt_f = (float)adc_value[4]*(3.3/4096);
 
-			printf(" %f %f %f\r\n", volt_0, volt_1, volt_2);
+			//printf(" %f %f %f\r\n", voltage[0].volt_f, voltage[1].volt_f, voltage[2].volt_f);
+			memcpy(usRegInputBuf,&voltage[0].volt_v[0], 4);
+			memcpy(&usRegInputBuf[2],&voltage[1].volt_v[0], 4);
+			memcpy(&usRegInputBuf[4],&voltage[2].volt_v[0], 4);
+
+			//printf(" %x %x %x %x %x %x\r\n\r\n",usRegInputBuf[0], usRegInputBuf[1], usRegInputBuf[2]\
+										,usRegInputBuf[3], usRegInputBuf[4], usRegInputBuf[5]);
+			
+			printf("--------- %f -----------\r\n",f_test.volt_f);
+			printf(" %x %x %x %x\r\n", f_test.volt_v[0], f_test.volt_v[1],f_test.volt_v[2],f_test.volt_v[3]);
+
+			memset(&usRegInputBuf[0], 0, sizeof(usRegInputBuf));
 
 			sample_finish = 0;
 			TIM_Cmd(TIM3, ENABLE);                     //完成周波采样，停止定时器  
